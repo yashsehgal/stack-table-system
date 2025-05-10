83eb7e4c7647f44f0f8af96225a10e0b8c39f277
Yash Sehgal <yashsehgal.work@gmail.com>
2025-05-10 09:55:49 +0530

    add responsive layout control wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { ResponsiveAutoAdapt } from '@/components/layout/responsive-auto-adapt'
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-});
+})
 
 export const metadata: Metadata = {
   title: 'Data-Table component',
   description: 'Data-table component for StackAI platform',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ResponsiveAutoAdapt>{children}</ResponsiveAutoAdapt>
+      </body>
     </html>
-  );
+  )
 }
--- components/layout/responsive-auto-adapt.tsx
+++ components/layout/responsive-auto-adapt.tsx
@@ -1,1 +1,8 @@
+import { cn } from '@/lib/utils'
+import { forwardRef } from 'react'
+
 export interface ResponsiveAutoAdaptProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const ResponsiveAutoAdapt = forwardRef<HTMLDivElement, ResponsiveAutoAdaptProps>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn('Responsive-auto-adapt p-4 max-md:p-2', className)} {...props} />
+})
